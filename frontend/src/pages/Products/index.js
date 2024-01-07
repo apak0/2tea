fdb163af75cda9130b3ee39cb2a57b73bdcf7069
Muhammed Apak <muhampak25@gmail.com>
2024-01-07 16:56:13 +0300

    changes from product pages

--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -1,64 +1,126 @@
-import React from "react";
-import { Box, Flex, Button } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Text, useToast } from "@chakra-ui/react";
 import Card from "../../components/Card";
 import { useInfiniteQuery } from "react-query";
-
-import { fetchProductList } from "../../api";
-
+import { fetchProductList, postOrder } from "../../api";
 import { motion } from "framer-motion";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useBasket } from "../../contexts/BasketContext";
 
-import "./style.css";
 
 function Products() {
-  const { data, error, status } = useInfiniteQuery(
-    "products",
-    fetchProductList,
-    {}
-  );
+  const { items, setItems } = useBasket();
+  const { user } = useAuth();
+  const [fullName, setFullName] = useState(user.fullname);
+  const [phoneNumber, setPhoneNumber] = useState(123);
+  const [address, setAddress] = useState("test3");
+  const { data, error, status } = useInfiniteQuery("products", fetchProductList, {});
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  useEffect(() => {
+    const anyItemHasQuantity = items.some((item) => item.quantity > 0);
+    setIsButtonDisabled(!anyItemHasQuantity);
+  }, [items]);
+
+  const { loggedIn } = useAuth();
+
+  const total = items.reduce((acc, item) => acc + item.quantity * item.price, 0);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const toastForOrder = () =>
+    toast({
+      title: "Order sended",
+      description: "Siparişiniz alındı...",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+    });
+
+  const handleSubmitForm = async () => {
+    const selectedItems = items.filter((item) => item.quantity > 0);
+    const itemIds = selectedItems.map((item) => item._id);
+    const input = {
+      fullName,
+      phoneNumber,
+      address,
+      items: JSON.stringify(itemIds),
+    };
+    await postOrder(input);
+    setTimeout(() => {
+      const updatedItems = items.map((item) => ({ ...item, quantity: 0 }));
+      setItems(updatedItems);
+    }, 400);
+    toastForOrder();
+  };
+
+  const handleNavigate = () => {
+    navigate("/signintoorder");
+  };
 
   if (status === "loading")
     return (
-      <Box
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        fontSize={"3xl"}
-        color={"cyan.400"}
-      >
-        {" "}
+      <Box display="flex" justifyContent="center" alignItems="center" fontSize="3xl" color="cyan.400">
         Loading...
       </Box>
     );
 
-  if (status === "error") return "An error has occurred: " + error.message;
+  if (status === "error") return <Box>An error has occurred: {error.message}</Box>;
 
   return (
-    <motion.Box
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      className=" items-center justify-center min-h-screen container sm:mx-0  "
-      py={5}
-    >
-      <Box
-        className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
-        m={10}
-      >
-        {data.pages.map((group, i) => (
-          <React.Fragment key={i}>
-            {group.map((item) => {
-              return (
-                <Box className="box" w={"100%"} rounded={"lg"} key={item._id}>
-                  <Card item={item} inBasket={false} />
-                </Box>
-              );
-            })}
-          </React.Fragment>
+    <motion.Box initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="flex items-center justify-center container sm:mx-0 " py={5}>
+      <Box className="flex gap-8" m={10}>
+        {items.map((item, i) => (
+          <Box key={i}>
+            <Box className="box" w="100%" rounded="lg" key={item._id}>
+              <Card item={item} inBasket={true} />
+            </Box>
+          </Box>
         ))}
       </Box>
+      <Box display="flex" flexDirection="column" alignSelf="baseline">
+        <Box
+          minW="100px"
+          border="2px"
+          borderColor="gray.300"
+          boxShadow="lg"
+          rounded="md"
+          bg="white"
+          display="flex"
+          flexDirection="column"
+          mt={10}
+          p={2}
+          mx="auto"
+        >
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Text textDecoration="underline" color="blue.500" fontSize="xl">
+              Ücret:
+            </Text>
+          </Box>
 
-      <Flex mt="10" justifyContent="center">
-        <Button m={5}>Sipariş ver</Button>
-      </Flex>
+          <Box display="flex" justifyContent="flex-end">
+            <Text as="b" color="orange.400" fontSize="lg">
+              {total} TL
+            </Text>
+          </Box>
+        </Box>
+
+        <Button
+          isDisabled={isButtonDisabled}
+          fontSize="2xl"
+          p={5}
+          mt="5"
+          size="sm"
+          bg="orange.400"
+          color="gray.100"
+          mx="auto"
+          onClick={() => {
+            loggedIn ? handleSubmitForm() : handleNavigate();
+          }}
+        >
+          Siparişi Gönder
+        </Button>
+      </Box>
     </motion.Box>
   );
 }
