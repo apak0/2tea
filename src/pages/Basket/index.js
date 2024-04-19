5bbbed42ecf1b7fcaefac7017fad36f231abc68c
Muhammed Apak <muhampak25@gmail.com>
2024-04-20 01:26:42 +0300

    backend server changed

--- src/pages/Basket/index.js
+++ src/pages/Basket/index.js
@@ -23,7 +23,7 @@ import "./styles.css";
 
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://twotea-backend.onrender.com");
 function Basket() {
   const [forceUpdate, setForceUpdate] = useState(false);
 
