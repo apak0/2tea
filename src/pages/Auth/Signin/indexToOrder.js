f7c677e874239d9e7809e6e0a5fb698e40282be9
Muhammed Apak <muhampak25@gmail.com>
2024-08-26 22:43:12 +0300

    sign-in to order page changed for new design

--- src/pages/Auth/Signin/indexToOrder.js
+++ src/pages/Auth/Signin/indexToOrder.js
@@ -8,6 +8,9 @@ import {
   Input,
   Button,
   Alert,
+  AlertTitle,
+  AlertDescription,
+  AlertIcon,
 } from "@chakra-ui/react";
 
 import { useFormik } from "formik";
@@ -16,10 +19,12 @@ import { fetchLogin } from "../../../api";
 
 import { useAuth } from "../../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function SigninToOrder() {
   const { login } = useAuth();
   const navigate = useNavigate();
+  const MotionBox = motion.div;
 
   const formik = useFormik({
     initialValues: {
@@ -42,49 +47,113 @@ function SigninToOrder() {
   });
 
   return (
-    <div>
-      <Flex aling="center" width="full" justifyContent="center">
-        <Box pt={10}>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <Flex align="center" width="full" justifyContent="center">
+        <Box pt={10} width={"300px"}>
           <Box textAlign="center">
-            <Heading>Giriş Yap</Heading>
+            <Heading
+              fontFamily={"lora"}
+              fontSize={"xxx-large"}
+              textColor={"#FE7A36"}
+            >
+              Giriş Yap
+            </Heading>
           </Box>
-          <Box my={5}>
+          <Box mt={5} my={5}>
             {formik.errors.general && (
               <Alert status="error">{formik.errors.general}</Alert>
             )}
           </Box>
           <Box my={5} textAlign="left">
             <form onSubmit={formik.handleSubmit}>
-              <FormControl>
+              <FormControl
+                isInvalid={formik.touched.email && formik.errors.email}
+              >
                 <FormLabel>E-mail</FormLabel>
                 <Input
                   name="email"
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.email}
-                  isInvalid={formik.touched.email && formik.errors.email}
                 />
+                {formik.errors.email && formik.touched.email && (
+                  <Alert
+                    mt={5}
+                    status="error"
+                    variant="left-accent"
+                    borderRadius="md"
+                    boxShadow="lg"
+                  >
+                    <AlertIcon />
+                    <MotionBox
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ duration: 0.5 }}
+                      flex="1"
+                    >
+                      <AlertTitle>Hata</AlertTitle>
+                      <AlertDescription>{formik.errors.email}</AlertDescription>
+                    </MotionBox>
+                  </Alert>
+                )}
               </FormControl>
-
-              <FormControl mt={4}>
+              <FormControl
+                mt={4}
+                isInvalid={formik.touched.password && formik.errors.password}
+              >
                 <FormLabel>Şifre</FormLabel>
                 <Input
                   type="password"
                   name="password"
                   onChange={formik.handleChange}
-                  onBlur={formik.onBlur}
+                  onBlur={formik.handleBlur}
                   value={formik.values.password}
                 />
-              </FormControl>
+                {formik.errors.password && formik.touched.password && (
+                  <Alert
+                    mt={5}
+                    status="error"
+                    variant="left-accent"
+                    borderRadius="md"
+                    boxShadow="lg"
+                  >
+                    <AlertIcon />
 
-              <Button mt={4} width="full" type="submit">
-                Giriş Yap
+                    <MotionBox
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ duration: 0.5 }}
+                      flex="1"
+                    >
+                      <AlertTitle>Hata</AlertTitle>
+                      <AlertDescription>
+                        {formik.errors.password}
+                      </AlertDescription>
+                    </MotionBox>
+                  </Alert>
+                )}
+              </FormControl>
+              <Button
+                borderRadius="50"
+                fontFamily="rocher"
+                style={{ background: "#F59E0B" }}
+                fontSize="xxx-large"
+                mt={4}
+                height="80px"
+                width="full"
+                type="submit"
+                _hover={{
+                  transform: "scale(1.1)",
+                  transition: "transform 0.2s ease-in-out",
+                }}
+              >
+                Devam Et
               </Button>
             </form>
           </Box>
         </Box>
       </Flex>
-    </div>
+    </motion.div>
   );
 }
 
